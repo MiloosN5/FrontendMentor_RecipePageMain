212ce6993e39bdb2f5699342702bca6fbc88d3fa
MiloosN5 <56539570+MiloosN5@users.noreply.github.com>
2024-06-11 18:18:12 +0200

    corr.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from './modules/shared/shared.module';
-import { AppRoutingModule } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SharedModule, AppRoutingModule],
+  imports: [CommonModule, RouterOutlet, SharedModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,3 @@ import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [];
 
-@NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: false })],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
